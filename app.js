837b86e70ce8121495700cee28a120f48b593b92
Sheila Yuridia Cruz Badillo <240314@utxicotepec.edu.mx>
2026-03-17 11:52:52 -0600

    Tienda-Virtual:
    Se instalaron las dependencias y librerias correspondientes

--- app.js
+++ app.js
@@ -3,23 +3,21 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const http = require('http');
-const { parse } = require('path');
 const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//*Configurar la ruta del servidor
-//*Ruta principal
-app.get('/', (req, res)=> res.status(200).send({
+// Ruta principal
+app.get('/', (req, res) => res.status(200).send({
     message: 'Bienvenido a mi API de tienda virtual',
 }));
 
-const port = parseInt(process.env.PORY, 10) || 8000;
+const port = process.env.PORT || 8000;
 app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port);
-moduule.exports =app;
+module.exports = app;
 
